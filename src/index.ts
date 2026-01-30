43e1bb9e5ba393fc5450683ce87f280be8b22364
Alex <alex.stormwood@bigfootds.com>
2026-01-31 10:35:43 +1100

    feat!: hello world express route initialised

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
+import { app } from "./server.js";
+
 
 console.log("Hello, world!");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+	console.log("Server is running on " + PORT);
+})
